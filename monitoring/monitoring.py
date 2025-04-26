622df8bcea422d8efb5c167d053aa759f0fe5934
sleepytmzd <depressionstonks420@gmail.com>
2025-04-26 16:53:05 +0600

    monitoring.py

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -19,6 +19,8 @@ from evidently.tests import TestEmbeddingsDrift
 from evidently.test_preset import DataDriftTestPreset, NoTargetPerformanceTestPreset
 from evidently.test_suite import TestSuite
 from evidently.ui.workspace.cloud import CloudWorkspace
+import boto3
+from botocore.config import Config
 
 load_dotenv()
 
@@ -46,15 +48,108 @@ DRIFT_THRESHOLD = 0.05  # 5% increase in distance means drift
 embedding_model = EmbeddingModels()
 qdrant_client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
 
+s3 = boto3.client(
+            "s3",
+            aws_access_key_id="AKIAWVZBUMBFCLDHCBHP",
+            aws_secret_access_key="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",
+            config=Config(signature_version="s3v4")
+        )
+
+def describe_product(row):
+    category = row.get('category', '') or ''
+    brand = row.get('brand', '') or ''
+    title = row.get('title', '') or ''
+    description = row.get('description', '') or ''
+    price = row.get('price', '') or ''
+    specs = row.get('specTableContent', '') or ''
+
+    parts = []
+    if title:
+        parts.append(f"The product is '{title}'")
+    else:
+        parts.append("This product")
+
+    if brand:
+        parts.append(f"from {brand}")
+
+    if category:
+        parts.append(f"in the {category} category")
+
+    sentence = " ".join(parts) + "."
+
+    if description:
+        sentence += f" Description: {description}"
+
+    if specs:
+        sentence += f" Specifications: {specs}"
+
+    if price:
+        sentence += f" It is priced at {price}."
+
+    return sentence.strip()
+
 # ---- Load Embeddings ---- #
 def get_query_embeddings():
-    with psycopg2.connect(**PG_CONN_PARAMS) as conn:
-        df = pd.read_sql("""
-            SELECT id, text, created_at
-            FROM user_queries
-        """, conn)
-        df["embedding"] = df["text"].apply(lambda x: embedding_model.get_dense_embeddings([x])[0])
-        return df
+    # with psycopg2.connect(**PG_CONN_PARAMS) as conn:
+    #     df = pd.read_sql("""
+    #         SELECT id, text, created_at
+    #         FROM user_queries
+    #         ORDER BY created_at ASC
+    #     """, conn)
+        
+    #     df["embedding"] = df["text"].apply(lambda x: embedding_model.get_dense_embeddings([x])[0])
+    #     return df
+    response = qdrant_client.scroll(collection_name="user_queries", limit=10000, with_vectors=True)
+    print(response)
+    points = response[0]
+    rows = []
+    for p in points:
+        payload = p.payload or {}
+        vectors = p.vector or {}
+        row = {
+            "id": p.id,
+            "timestamp": payload.get("created_at"),
+            "dense_vector": vectors.get("text-dense")
+        }
+        rows.append(row)
+
+    print(rows)
+    df = pd.DataFrame(rows)
+    df_sorted = df.sort_values(by="created_at", ascending=True).reset_index(drop=True)
+    return df_sorted
+
+def get_product_embeddings():
+    # with psycopg2.connect(**PG_CONN_PARAMS) as conn:
+    #     df = pd.read_sql("""
+    #         SELECT *
+    #         FROM products
+    #         ORDER BY created_at ASC
+    #     """, conn)
+    #     # df_text = pd.DataFrame()
+    #     df["text"] = df.apply(describe_product, axis=1)
+    #     df["embedding"] = df["text"].apply(lambda x: embedding_model.get_dense_embeddings([x])[0])
+    #     return df
+    response = qdrant_client.scroll(collection_name="products", limit=10000, with_vectors=True, with_payload=True)
+    points = response[0]
+    rows = []
+    # i = 0
+    for p in points:
+        payload = p.payload or {}
+        vectors = p.vector or {}
+        # if i < 1:
+        #     print(p.payload)
+        #     i += 1
+        row = {
+            "id": p.id,
+            "timestamp": payload.get("created_at"),
+            "dense_vector": vectors.get("text-dense"),
+            "sparse_vector": vectors.get("text-sparse")
+        }
+        rows.append(row)
+
+    df = pd.DataFrame(rows)
+    df_sorted = df.sort_values(by="timestamp", ascending=True).reset_index(drop=True)
+    return df_sorted
     
 def get_product_centroid():
     response = qdrant_client.scroll(collection_name="products", limit=10000, with_vectors=True)
@@ -67,7 +162,8 @@ def compute_avg_cosine_distance(vectors, reference):
 
 # ---- Drift Calculation ---- #
 def calculate_and_log_drift():
-    df = get_query_embeddings()
+    # query_df = get_query_embeddings()
+    product_df = get_product_embeddings()
     product_centroid = get_product_centroid()
 
     # cutoff = datetime.utcnow() - timedelta(days=QUERY_LOOKBACK_DAYS)
@@ -77,46 +173,81 @@ def calculate_and_log_drift():
     # if len(historical) < 10 or len(recent) < 10:
     #     print("Not enough data to compute drift.")
     #     return
-    historical = df[:60]
-    recent = df[60:]
+    # historical_query = query_df[:60]
+    # recent_query = query_df[60:]
+    # hist_query_vectors = np.stack(historical_query["embedding"].values)
+    # recent_query_vectors = np.stack(recent_query["embedding"].values)
 
-    hist_vectors = np.stack(historical["embedding"].values)
-    recent_vectors = np.stack(recent["embedding"].values)
+    # hist_mean = np.mean(hist_query_vectors, axis=0)
+    # recent_mean = np.mean(recent_query_vectors, axis=0)
 
-    hist_mean = np.mean(hist_vectors, axis=0)
-    recent_mean = np.mean(recent_vectors, axis=0)
+    # l2_drift = np.linalg.norm(hist_mean - recent_mean)
+    # print(f"L2 drift between historical and recent mean embeddings: {l2_drift:.4f}")
 
-    # L2 drift
-    l2_drift = np.linalg.norm(hist_mean - recent_mean)
-    print(f"L2 drift between historical and recent mean embeddings: {l2_drift:.4f}")
+    # hist_cos = compute_avg_cosine_distance(hist_query_vectors, product_centroid)
+    # recent_cos = compute_avg_cosine_distance(recent_query_vectors, product_centroid)
+    # cos_drift = recent_cos - hist_cos
+    # print(f"Mean cosine distance drift (recent - historical): {cos_drift:.4f}")
 
-    # Cosine centroid drift
-    hist_cos = compute_avg_cosine_distance(hist_vectors, product_centroid)
-    recent_cos = compute_avg_cosine_distance(recent_vectors, product_centroid)
-    cos_drift = recent_cos - hist_cos
-    print(f"Mean cosine distance drift (recent - historical): {cos_drift:.4f}")
+    # if cos_drift > DRIFT_THRESHOLD:
+    #     print("⚠️ Drift detected. Consider triggering model retraining.")
+    # else:
+    #     print("✅ No significant drift detected.")
 
-    if cos_drift > DRIFT_THRESHOLD:
-        print("⚠️ Drift detected. Consider triggering model retraining.")
-    else:
-        print("✅ No significant drift detected.")
+    # hist_query_embed_df = pd.DataFrame(hist_query_vectors, columns=[f"dim_{i}" for i in range(hist_query_vectors.shape[1])])
+    # recent_query_embed_df = pd.DataFrame(recent_query_vectors, columns=[f"dim_{i}" for i in range(recent_query_vectors.shape[1])])
+    
+    # column_mapping = ColumnMapping(
+    #     embeddings={"full_embedding": hist_query_embed_df.columns.tolist()}
+    # )
 
-    # suite = TestSuite(tests=[EmbeddingDriftPreset()])
-    # suite.run(reference_data=historical, current_data=recent)
-    # suite.save_html("embedding_drift_report.html")
+    # report = Report(metrics=[
+    #     EmbeddingsDriftMetric("full_embedding")
+    # ])
+    # report.run(reference_data=hist_query_embed_df, current_data=recent_query_embed_df, column_mapping=column_mapping)
+    # report.save_html("query_embedding_drift_report.html")
 
-    hist_embed_df = pd.DataFrame(hist_vectors, columns=[f"dim_{i}" for i in range(hist_vectors.shape[1])])
-    recent_embed_df = pd.DataFrame(recent_vectors, columns=[f"dim_{i}" for i in range(recent_vectors.shape[1])])
+    hist_product = product_df[:8000]
+    recent_product = product_df[8000:]
+    hist_product_vectors = np.stack(hist_product["dense_vector"].values)
+    recent_product_vectors = np.stack(recent_product["dense_vector"].values)
+    hist_product_embed_df = pd.DataFrame(hist_product_vectors, columns=[f"dim_{i}" for i in range(hist_product_vectors.shape[1])])
+    recent_product_embed_df = pd.DataFrame(recent_product_vectors, columns=[f"dim_{i}" for i in range(recent_product_vectors.shape[1])])
     
     column_mapping = ColumnMapping(
-        embeddings={"full_embedding": hist_embed_df.columns.tolist()}
+        embeddings={"full_embedding": hist_product_embed_df.columns.tolist()}
     )
 
     report = Report(metrics=[
         EmbeddingsDriftMetric("full_embedding")
     ])
-    report.run(reference_data=hist_embed_df, current_data=recent_embed_df, column_mapping=column_mapping)
-    report.save_html("embedding_drift_report.html")
+    report.run(reference_data=hist_product_embed_df, current_data=recent_product_embed_df, column_mapping=column_mapping)
+    result = report.as_dict()
+    result = result["metrics"][0]["result"]["drift_detected"]
+    print(result)
+    report.save_html("product_embedding_drift_report.html")
+
+    # with open("query_embedding_drift_report.html", 'rb') as file_data:
+    #     html_content = file_data.read()
+    # bucket_name = 'poridhi-manat-bucket'
+    # s3_key = 'reports/query_embedding_drift_report.html'
+
+    # try:
+    #     s3.put_object(Bucket=bucket_name, Key=s3_key, Body=html_content, ContentType='text/html')
+    # except Exception as e:
+    #     print(f"Failed to upload file : {e}")
+
+    with open("product_embedding_drift_report.html", 'rb') as file_data:
+        html_content = file_data.read()
+    bucket_name = 'poridhi-manat-bucket'
+    s3_key = 'reports/product_embedding_drift_report.html'
+
+    try:
+        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=html_content, ContentType='text/html')
+    except Exception as e:
+        print(f"Failed to upload file : {e}")
+
+
 
 if __name__ == "__main__":
     calculate_and_log_drift()
